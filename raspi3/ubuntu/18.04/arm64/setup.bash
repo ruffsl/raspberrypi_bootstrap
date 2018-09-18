57c41f7b02e055faa2b6311ba3cdf43a0fc26930
ruffsl <roxfoxpox@gmail.com>
2018-09-17 18:16:51 -0700

    Sorting paths with envs

--- raspi3/ubuntu/18.04/arm64/setup.bash
+++ raspi3/ubuntu/18.04/arm64/setup.bash
@@ -1,103 +1,99 @@
 #!/bin/bash
 # Construct a arm64+raspi2 installer.
 
+# https://www.raspberrypi.org/forums/viewtopic.php?t=220079
+# https://lb.raspberrypi.org/forums/viewtopic.php?t=215438
+# https://wiki.ubuntu.com/ARM/RaspberryPi#Building_Raspberry_Pi_3_images
+
 # set -e
 
 source setup.env
 build_root=`pwd`
 
+
+mkdir -p $cache_dir
+
+rm -rf $target_dir
+rm -rf $scratch_dir
+
+mkdir -p $target_dir
+mkdir -p $scratch_dir
+
+cd $cache_dir
+
 # Download the iso
 wget -N "http://cdimage.ubuntu.com/ubuntu/releases/$release/release/$iso_name"
-
 # Extract the files from the iso to a directory server-raspi3
-7z x -oserver-raspi3 "$iso_name"
+7z x -o$target_dir "$iso_name"
 
 # Download the latest pi bootloader files
 wget -N "http://archive.raspberrypi.org/debian/pool/main/r/raspberrypi-firmware/$raspberrypi_bootloader"
-dpkg-deb -x $raspberrypi_bootloader /tmp/pi-bootloader
-
-cd server-raspi3
-
+dpkg-deb -x $raspberrypi_bootloader /tmp/raspberrypi_bootloader
 # Copy the bootloader files
-cp /tmp/pi-bootloader/boot/* .
+cp /tmp/raspberrypi_bootloader/boot/* $target_dir
 
-# The cmdline.txt (adjust as necessary)
-cp $build_root/resources/cmdline.txt .
+# The cmdline.txt, config.txt preseed files, (adjust as necessary)
+cp -r $build_root/resources/* $target_dir
 
-# The config.txt
-cp $build_root/resources/config.txt .
-
-# Run 'unix2dos config.txt' if you want it readable in windows
-
-# Create preseed files
-mkdir -p preseed
-cd preseed
-cp $build_root/resources/preseed/pi.seed .
-
-
-mkdir -p raspberry
-cd raspberry
-
-
-cp $build_root/resources/preseed/raspberry/pi_recipe .
-
-cp $build_root/resources/preseed/raspberry/pi_partman_command .
-
-cp $build_root/resources/preseed/raspberry/pi_late_command .
 
 # Download wifi firmware
-mkdir -p wifi-firmware
-cd wifi-firmware
+mkdir -p $cache_dir/wifi-firmware
+cd $cache_dir/wifi-firmware
 # Pi 3B
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.bin
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.txt
+wget -N "https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.bin"
+wget -N "https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.txt"
 # Pi 3B+
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.bin
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.clm_blob
-wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.txt
+wget -N "https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.bin"
+wget -N "https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.clm_blob"
+wget -N "https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43455-sdio.txt"
+cp -r $cache_dir/wifi-firmware $target_dir/preseed/raspberry
+
 
 # Download raspi2 kernel debs (these probably should be saved in pool)
-cd ..
-mkdir -p kernel-debs
-cd kernel-debs
-
+mkdir -p $cache_dir/kernel-debs
+mkdir -p $scratch_dir/pi-kernel
+cd $cache_dir/kernel-debs
 for i in "${linux_packages[@]}"
 do
 	 wget -N "http://ports.ubuntu.com/ubuntu-ports/pool/universe/l/linux-raspi2/$i";
 	 # Extract vmlinuz-raspi2 and dtb files
-	 dpkg-deb -x $i /tmp/pi-kernel
+	 dpkg-deb -x $i $scratch_dir/pi-kernel
 done
 
-# dpkg-deb -x *.deb /tmp/pi-kernel
-cd ../../../
-version="$(ls /tmp/pi-kernel/boot/vmlinuz* | sed 's,^\/tmp\/pi-kernel\/boot\/vmlinuz-,,g')"
-cp /tmp/pi-kernel/lib/firmware/$version/device-tree/broadcom/bcm2710*.dtb .
-cp /tmp/pi-kernel/boot/vmlinuz-$version install/vmlinuz-raspi2
+version=$(ls $scratch_dir/pi-kernel/boot/vmlinuz*)
+version=${version##*/}
+version=${version#"vmlinuz-"}
+echo version=$version
+
+mkdir -p $target_dir/dtb
+cp -r $scratch_dir/pi-kernel/lib/firmware/$version/device-tree/broadcom $target_dir/dtb
+cp -r $scratch_dir/pi-kernel/boot/vmlinuz-$version $target_dir/install/vmlinuz-raspi2
 
 # Create raspi2 initrd
-zcat -k install/initrd.gz > install/initrd-raspi2
+zcat -k $target_dir/install/initrd.gz > $target_dir/install/initrd-raspi2
 
 # Prepare to include wifi-firmware in initrd
-mkdir -p /tmp/pi-kernel/lib/firmware/brcm/
-chmod 755 /tmp/pi-kernel/lib/firmware/brcm/
-cp preseed/raspberry/wifi-firmware/*sdio* /tmp/pi-kernel/lib/firmware/brcm/
-chmod 644 /tmp/pi-kernel/lib/firmware/brcm/*
+mkdir -p $scratch_dir/pi-kernel/lib/firmware/brcm/
+chmod 755 $scratch_dir/pi-kernel/lib/firmware/brcm/
+cp $target_dir/preseed/raspberry/wifi-firmware/*sdio* $scratch_dir/pi-kernel/lib/firmware/brcm/
+chmod 644 $scratch_dir/pi-kernel/lib/firmware/brcm/*
 
 # evbug spams the logs, get rid
-rm /tmp/pi-kernel/lib/modules/$version/kernel/drivers/input/evbug.ko || true
+rm $scratch_dir/pi-kernel/lib/modules/$version/kernel/drivers/input/evbug.ko || true
 
 # Generate modules.dep etc files
-depmod -a -b /tmp/pi-kernel -F /tmp/pi-kernel/boot/System.map-$version $version
+cd $target_dir
+depmod -a -b $scratch_dir/pi-kernel -F $scratch_dir/pi-kernel/boot/System.map-$version $version
 
-# Remove unneeded files
-rm -r /tmp/pi-kernel/lib/modules/$version/initrd || true
-rm -r /tmp/pi-kernel/boot
-rm -r /tmp/pi-kernel/usr
-rm -r /tmp/pi-kernel/lib/firmware/$version
+# # Remove unneeded files
+rm -r $scratch_dir/pi-kernel/lib/modules/$version/initrd || true
+rm -r $scratch_dir/pi-kernel/boot
+rm -r $scratch_dir/pi-kernel/usr
+rm -r $scratch_dir/pi-kernel/lib/firmware/$version
 
 # Add raspi2 modules/firmware to the initrd and compress
-find /tmp/pi-kernel/.  | sed 's,^/tmp/pi-kernel\/,,g'| cpio -D /tmp/pi-kernel -R +0:+0 -H newc -o -A -F install/initrd-raspi2
-gzip install/initrd-raspi2
+find $scratch_dir/pi-kernel/.  | sed 's,^/tmp/scratch/pi-kernel\/,,g'| cpio -D $scratch_dir/pi-kernel -R +0:+0 -H newc -o -A -F $target_dir/install/initrd-raspi2
+gzip $target_dir/install/initrd-raspi2
 
 echo "Finished!"
 echo "Copy all files (including the hidden .disk folder) in the server-raspi3 folder to a fat formatted usb drive."
